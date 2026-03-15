103800cc0c2b915d62790e12aa7a4e516d0d23b9
Kosty Manuhin <kostymanuhin@gmail.com>
2026-03-15 15:47:10 +0300

    Вывод ошибок

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import sys
+import traceback
 
 from PyQt5.QtWidgets import QApplication
 
 from app.config import APP_NAME, VERSION, APP_STYLES
 from app.ui.main_window import MainWindow
 
+def exception_hook(exctype, value, tb):
+    print('\n'.join(traceback.format_exception(exctype, value, tb)))
 
 def main():
+    sys.excepthook = exception_hook
+
     app = QApplication(sys.argv)
     app.setApplicationName(APP_NAME)
     app.setApplicationVersion(VERSION)
